6bdc104260ce0815c337076e011b249adfc20aca
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2026-06-10 16:44:14 -0300

    configurando o worker para repetir conexão caso o rabbitmq ainda não esteja pronto, criando dockerfile e serviços no docker compose (+api, +worker)

--- backend/src/queue/rabbitmq.ts
+++ backend/src/queue/rabbitmq.ts
@@ -3,8 +3,19 @@ import "dotenv/config"
 
 export async function connectRabbit() {
 
-    const connection = await amqp.connect(process.env.RABBITMQ_URL!)
-    const channel = await connection.createChannel()
+    while (true) {
 
-    return channel
+        try {
+            const connection = await amqp.connect(process.env.RABBITMQ_URL!)
+            const channel = await connection.createChannel()
+    
+            console.log("CONNECTED TO RABBITMQ!");
+            
+            return channel
+        } catch (error) {
+
+            console.log("RABBITMQ STILL NO WORKING, TRYING AGAIN...");
+            await new Promise(resolve => setTimeout(resolve, 5000))
+        }
+    }
 }
